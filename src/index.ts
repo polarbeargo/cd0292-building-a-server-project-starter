6281f8aa892ef44db7de8f5a8cec3041a37aeb64
Hsin-Wen Chang <bow1226@gmail.com>
2024-04-28 16:24:34 +0800

    refactor: Refactor app.ts into index.ts replace Log module with morgan

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,32 @@
-import app from './app';
-const port = 3000;
+import Storage from './storage';
+import routes from './routes/index';
+import ErrorHandle from './middleware';
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
 
-app.listen(port, () => {
+const port = 3002;
+
+const app: express.Application = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('combined'));
+app.use(ErrorHandle);
+
+// Routes
+app.use('/', routes);
+
+// Health Check
+app.get('/health', (req, res) => {
+  res.status(200).json({status: 'OK'});
+});
+
+app.listen(port, async (): Promise<void> => {
+  await Storage.createThumbnailPath();
   console.log(`Server is listening on port ${port}`);
 });
+
+export default app;
--- src/middleware/Log.ts
+++
@@ -1,8 +0,0 @@
-import {Request, Response, NextFunction} from 'express';
-// createlogger middleware function
-function logger(req: Request, res: Response, next: NextFunction) {
-  console.log(`Request URL: ${req.url}`);
-  next();
-}
-
-export default logger;
