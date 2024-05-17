d3117e4839aeb16c28cb053cf7eb2db22734788f
Hsin-Wen Chang <bow1226@gmail.com>
2024-05-17 20:14:14 +0800

    feat: Implement  fetch ImageThumbnailPath and resizeImageAPI in React

--- client/my-app/src/App.tsx
+++ client/my-app/src/App.tsx
@@ -1,8 +1,56 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+import Button from '@material-ui/core/Button';
 
 function App() {
+  const [thumbnails, setThumbnails] = useState([]);
+  const [state, formAction] = useState({
+    width: '',
+    height: 0,
+    fileName: 0,
+  });
+
+  const [item, setItem] = useState('ImageThumbnailPath');
+
+  const fetchThumb = async () => {
+    const url = 'http://localhost:3002/api/ImageThumbnailPath';
+    const response = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    });
+    return await response.json();
+  };
+
+  const resizeImageAPI = async () => {
+    const url = `http://localhost:3002/api/ImageProcessing?filename=${state.fileName}&width=${state.width}&height=${state.height}`;
+    const response = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    });
+    return await response.json();
+  };
+
+  const resizeImage = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    resizeImageAPI().then(response => {
+      console.log(response);
+    });
+  };
+
+  const thumbnailsShow = (): void => {
+    fetchThumb().then(response => {
+      setThumbnails(response.thumbnails);
+    });
+  };
+
+  useEffect(thumbnailsShow, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +66,15 @@ function App() {
         >
           Learn React
         </a>
+        <Button variant="contained" color="primary" onClick={resizeImage}>
+          Resize Image
+        </Button>
+        <Button variant="contained" color="secondary" onClick={thumbnailsShow}>
+          Show Thumbnails
+        </Button>
+        {thumbnails.map((thumbnail: string, index: number) => (
+          <img key={index} src={thumbnail} alt={`Thumbnail ${index}`} />
+        ))}
       </header>
     </div>
   );
